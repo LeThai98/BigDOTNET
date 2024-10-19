e7d5141185ffb0528879bb736edab8ecc17c32d7
LeThai98 <hongthaifamily@gmail.com>
2024-10-19 13:57:47 +0700

    Pipeline Flow with Use(), Map(), Run() extension

---
+++ Middleware/Middleware_README.md
@@ -0,0 +1,7 @@
+# Features
+
+## 1. Using the Use, Map, Run extension method
+- Create Middleware Pipeline by using Use,Run extensions
+- Understanding Pipeline flow
+- Create the sub-pipeline or new branch pipeline with Map() extension when handling specific Url
+
--- Middleware/Program.cs
+++ Middleware/Program.cs
@@ -22,4 +22,34 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+/*
+    Use Use, Map, and Run extension methods to add middleware to the request pipeline.
+*/
+
+// Middleware using Use()
+app.Use(async (context, next) =>
+        {
+            // Do something before the next middleware
+            await context.Response.WriteAsync("Middleware 1: Before\n");
+            await next.Invoke();
+            // Do something after the next middleware
+            await context.Response.WriteAsync("Middleware 1: After\n");
+        });
+
+// Middleware using Map()
+// Map defines a sub-pipeline - new branch in the pipeline => it will not call the next middleware after it 
+app.Map("/special", subApp =>
+{
+    subApp.Run(async context =>
+    {
+        await context.Response.WriteAsync("Mapped Middleware: This is a special path!\n");
+    });
+});
+
+// Middleware using Run() - This is the Terminate middleware in the pipeline
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Run Middleware: This is the final handler.\n");
+});
+
 app.Run();
